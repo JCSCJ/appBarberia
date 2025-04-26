c5532e4c1b9a3c745365ef757f73f1aeb964285f
JCSCJ <josclopcz@gmail.com>
2025-04-26 15:13:36 -0500

    Productos, rutas,carrito de compras

--- app/src/main/java/com/example/appbarberia/LoginActivity.kt
+++ app/src/main/java/com/example/appbarberia/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.example.appbarberia
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,19 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnCreateAccount = findViewById<Button>(R.id.btnCreateAccount)
+
+        btnCreateAccount.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+
         }
     }
 }
--- app/src/main/java/com/example/appbarberia/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/appbarberia/ui/notifications/NotificationsFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.appbarberia.databinding.FragmentNotificationsBinding
+import com.example.appbarberia.databinding.FragmentAccountBinding
 
 class NotificationsFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentAccountBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,10 +25,10 @@ class NotificationsFragment : Fragment() {
         val notificationsViewModel =
             ViewModelProvider(this)[NotificationsViewModel::class.java]
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentAccountBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
+        val textView: TextView = binding.textAccount
         notificationsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
