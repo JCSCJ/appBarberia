ee814110ae775a0ee29759865f72383ebe270939
JCSCJ <josclopcz@gmail.com>
2025-05-15 22:31:35 -0500

    Arreglo de navController

--- app/src/main/java/com/example/appbarberia/HomeActivity.kt
+++ app/src/main/java/com/example/appbarberia/HomeActivity.kt
@@ -1,17 +1,12 @@
 package com.example.appbarberia
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.appbarberia.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
-
-    
 
     private lateinit var binding: ActivityHomeBinding
 
@@ -21,19 +16,10 @@ class HomeActivity : AppCompatActivity() {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_activity_home) as NavHostFragment
+        val navController = navHostFragment.navController
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_home)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        binding.navView.setupWithNavController(navController)
     }
-
-
 }
