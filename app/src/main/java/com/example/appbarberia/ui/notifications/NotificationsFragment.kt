48dd70a86cb5a487127937eb252236ae65f25192
JCSCJ <josclopcz@gmail.com>
2025-05-30 18:01:39 -0500

    backgrounds and designs

--- app/src/main/java/com/example/appbarberia/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/appbarberia/ui/notifications/NotificationsFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.example.appbarberia.databinding.FragmentAccountBinding
 
 class NotificationsFragment : Fragment() {
@@ -22,16 +20,12 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this)[NotificationsViewModel::class.java]
+
 
         _binding = FragmentAccountBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textAccount
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+
         return root
     }
 
